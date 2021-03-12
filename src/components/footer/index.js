55307e82f286b131e4ba619fdafbae24256e958a
Justin Ng <justinng123409@yahoo.com>
2021-03-11 16:33:50 -0800

    added links to footer icons

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,10 +7,10 @@ function Footer(props) {
         <div className='footer'>
             Follow Us:
             <div className='footer-icons'>
-                <a href={props.link}>
+                <a href={props.link1}>
                     <img src={props.icon1} className='footer-icon'/>       
                 </a>
-                <a href={props.link}>
+                <a href={props.link2}>
                     <img src={props.icon2} className='footer-icon'/>                    
                 </a>
             </div>
